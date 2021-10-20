208556a85929214b839de25b61071799d6621f51
Brian Cain <bcain@hashicorp.com>
2021-10-20 14:23:17 -0700

    Add auth to registry plugin

--- plugins/gobuilder_final/registry/auth.go
+++ plugins/gobuilder_final/registry/auth.go
@@ -1,16 +1,21 @@
 package registry
 
-// Comment out the following functions to implement Authentication in
-// your component
+import (
+	"context"
+	"fmt"
 
-//func (r *Registry) ValidateAuthFunc() interface{} {
-//	return r.validateAuth
-//}
-//
-//// AuthFunc satisfies the Authenticator interface
-//func (r *Registry) AuthFunc() interface{} {
-//	return r.authenticate
-//}
+	"github.com/hashicorp/waypoint-plugin-sdk/component"
+	"github.com/hashicorp/waypoint-plugin-sdk/terminal"
+)
+
+func (r *Registry) ValidateAuthFunc() interface{} {
+	return r.validateAuth
+}
+
+// AuthFunc satisfies the Authenticator interface
+func (r *Registry) AuthFunc() interface{} {
+	return r.authenticate
+}
 
 // A ValidateAuthFunc does not have a strict signature, you can define the parameters
 // you need based on the Available parameters that the Waypoint SDK provides.
@@ -22,25 +27,22 @@ package registry
 // - *component.Source
 // - *component.JobInfo
 // - *component.DeploymentConfig
-// - *datadir.Project
-// - *datadir.App
-// - *datadir.Component
 // - hclog.Logger
 // - terminal.UI
 // - *component.LabelSet
 //
 // If an error is returned, Waypoint will attempt to call
 // AuthFunc
-//func (r *Registry) validateAuth(
-//	ctx context.Context,
-//	ui terminal.UI,
-//) error {
-//	s := ui.Status()
-//	defer s.Close()
-//	s.Update("Validate authentication")
-//
-//	return fmt.Errorf("Unable to Authenticate")
-//}
+func (r *Registry) validateAuth(
+	ctx context.Context,
+	ui terminal.UI,
+) error {
+	s := ui.Status()
+	defer s.Close()
+	s.Update("Validate authentication")
+
+	return fmt.Errorf("Unable to Authenticate")
+}
 
 // A AuthFunc does not have a strict signature, you can define the parameters
 // you need based on the Available parameters that the Waypoint SDK provides.
@@ -52,20 +54,18 @@ package registry
 // - *component.Source
 // - *component.JobInfo
 // - *component.DeploymentConfig
-// - *datadir.Project
-// - *datadir.App
-// - *datadir.Component
 // - hclog.Logger
 // - terminal.UI
 // - *component.LabelSet
 //
 // Output parameters must be *component.AuthResult, error
-//func (p *Registry) authenticate(
-//	ctx context.Context,
-//	ui terminal.UI,
-//) (*component.AuthResult, error) {
-//
-//	ui.Output("Describe the manual authentication steps here")
-//
-//	return &component.AuthResult{Authenticated: false}, nil
-//}
+func (p *Registry) authenticate(
+	ctx context.Context,
+	ui terminal.UI,
+) (*component.AuthResult, error) {
+	s := ui.Status()
+	defer s.Close()
+	s.Update("Describe the manual authentication steps here")
+
+	return &component.AuthResult{Authenticated: false}, nil
+}
